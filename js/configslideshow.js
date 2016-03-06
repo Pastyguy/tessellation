4a3782851ae7977df1e85927c048b6be5cf36950
Pastyguy <bibblefirman@gmail.com>
2016-03-05 20:09:09 -0800

    initial upload
    added all the files for website

---
+++ js/configslideshow.js
@@ -0,0 +1,17 @@
+
+var opts = {
+    auto : {
+        speed : 3500, 
+        pauseOnHover : true
+    },
+    fullScreen : false, 
+    swipe : true
+};
+makeBSS('.num1', opts);
+
+var opts2 = {
+    auto : false,
+    fullScreen : true,
+    swipe : true
+};
+makeBSS('.num2', opts2);
